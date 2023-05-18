954e0cce48bb27fe544ad6d6bd555eb981c18477
Koston Zhuang <koston.zhuang@maiscrm.com>
2023-05-18 20:58:19 +0800

    refine json

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@
 // import { initWSClient } from './gocq';
 // import { SendListFriendsRequest } from './gocq/action';
 
-import { JSONWithCustomFields, marshal } from "./util/json";
 
 // const ws = initWSClient();
 
@@ -18,30 +17,3 @@ import { JSONWithCustomFields, marshal } from "./util/json";
 //     SendListFriendsRequest();
 // }, 3000);
 
-class A implements JSONWithCustomFields {
-    getMarshalNameMap(): Map<string, string> {
-        const m = new Map<string, string>();
-        m.set('userId', "user_id");
-        m.set('data', 'wuhu');
-        m.set('data.autoEscape', 'auto_escape');
-        return m;
-    }
-    getUnMarshalNameMap(): Map<string, string> {
-        const m = new Map<string, string>();
-        return m;
-    }
-    userId: string;
-    data: B;
-}
-
-class B {
-    autoEscape: boolean;
-}
-
-const a = new A();
-a.userId = '1658272229';
-a.data = {
-    autoEscape: true,
-}
-
-console.log(marshal(a))
